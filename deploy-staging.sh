1e2ab7fbd9451143a4b2d76bee9c1e2607ff9238
jorisgrls <jorisgrilleres@gmail.com>
2024-03-16 17:14:42 +0100

    fix

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -3,8 +3,3 @@ git fetch origin && git reset --hard origin/staging && git clean -f -d && \
 docker compose -f docker-compose.staging.yml down && \
 docker compose -f docker-compose.staging.yml pull && \
 docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
-
-
-
-zeyfgzeigfyzegfyuzegfzefhiuzehfiouzeehfuizehf
-zeyfgzeigfyzegfyuzegfzefhiuzehfiouzeehfuizehf
